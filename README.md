30f4eb849e190134bb62525887e54f9add01f061
Amor Bounechada <amor.bounechada@jetpulp.fr>
2017-10-06 11:46:19 +0200

    add readme.md

---
+++ README.md
@@ -0,0 +1,30 @@
+ **/ Small project to understand gulp
+
+- create a local web server
+- compiling Sass in CSS
+- refresh the browser automatically whenever a file is modified
+- optimize all resources (CSS, JS, fonts, images).
+
+** You will need Node.js on your pc **
+
+- in a console type
+  - $ npm install gulp -g
+
+- creates a package.json file  cd/Project and type
+    - $ npm init
+
+- type npm install gulp --save-dev "we install Gulp in the project folder and not globally"
+
+
+- installation of sass "we install Sass in the project folder and not globally""
+    -$ npm install gulp-sass --save-dev
+
+** / it is necessary to install all the mentioned components in the gulpfile.js
+
+
+
+
+
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,28 @@ var sass = require('gulp-sass');
 // Require the browser sync
 var browserSync = require('browser-sync');
 
-//
+// Require useref
+var useref = require('gulp-useref');
+
+// Require gulp-uglify
+var uglify = require('gulp-uglify');
+
+// Require gulpIf
+var gulpIf = require('gulp-if');
+
+// Require gulp-mimify-css
+var minifyCSS = require('gulp-minify-css');
+
+// Require gulp-imagemin
+var imagemin = require('gulp-imagemin');
+
+// Require gulp-cache
+var cache = require('gulp-cache');
+
+// require del
+var del = require('del');
+
+// Test function
 gulp.task('hello', function() {
     console.log('Hello Zell');
 });
@@ -24,7 +45,7 @@ gulp.task('sass', function ()  {
         }))
 });
 
-// Add  browser sync
+// Live Refresh with Browser Sync
 gulp.task('browserSync', function () {
     browserSync({
         server: {
@@ -33,10 +54,97 @@ gulp.task('browserSync', function () {
     })
 });
 
-// add watcher
+// add watcher - Observe file changes
 gulp.task('watch', ['browserSync', 'sass'], function () {
     gulp.watch('app/scss/**/*.scss', ['sass']);
     // Reload the browser whenever HTML or JS files change
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
 });
+
+// Optimize CSS and JavaScript
+gulp.task('useref', function(){
+    var assets = useref.assets();
+
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('useref', function(){
+    var assets = useref.assets();
+
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe(uglify()) // to Minify Javascript Files
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('useref', function(){
+    var assets = useref.assets();
+
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        // Minimize only Javascript files
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('useref', function(){
+    var assets = useref.assets();
+
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        // Minifie seulement les fichiers CSS
+        .pipe(gulpIf('*.css', minifyCSS()))
+        // Minifie seulement les fichiers Javascript
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+        .pipe(imagemin({
+            interlaced: true
+        }))
+        .pipe(gulp.dest('dist/images'))
+});
+
+// Optimize images
+gulp.task('images', function(){
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Met en cache les images passées par imagemin
+        .pipe(cache(imagemin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+});
+
+// We can copy files with Gulp without plugin
+gulp.task('fonts', function() {
+    return gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+});
+
+// Automatically clean generated files
+gulp.task('clean', function() {
+    del('dist');
+});
+
+gulp.task('clean:dist', function(callback){
+    del(['dist/**/*', '!dist/images', '!dist/images/**/*'], callback)
+});
+
+gulp.task('clean', function(callback) {
+    del('dist');
+    return cache.clearAll(callback);
+});
+
